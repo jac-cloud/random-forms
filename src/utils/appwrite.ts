4d736da50ccbcb707cb6c2b1afce8d593c29191e
emanuele-toma <toma.emanuele03@gmail.com>
2024-10-17 13:34:20 +0200

    chore: add appwrite endpoint and project

--- src/utils/appwrite.ts
+++ src/utils/appwrite.ts
@@ -1,4 +1,4 @@
 import { Client } from 'appwrite';
 
-export const client = new Client();
+export const client = new Client().setEndpoint('https://cloud.appwrite.io/v1').setProject('6710ef3b003a2dd7daf6');
 client.setProject('6710ef3b003a2dd7daf6');
