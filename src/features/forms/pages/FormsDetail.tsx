05326a21d2624b749c0a30f4e65c1b24c812f048
Minnei Mirko <mirko@minnei.it>
2024-10-23 15:25:00 +0200

    refactor: cleanup

--- src/features/forms/pages/FormsDetail.tsx
+++ src/features/forms/pages/FormsDetail.tsx
@@ -53,52 +53,11 @@ export function FormsDetail() {
 
   const [quiz, setQuiz] = useState<FormsDetails | null>(null);
   const [error, setError] = useState<Error | null>(null);
-  const [givenAnswers, setGivenAnswers] = useState<{ [key: string]: string | undefined }>({});
   const [submitting, setSubmitting] = useState(false);
 
-  useEffect(() => {
-    try {
-      getForm(quizId)
-        .then(qs => {
-          setQuiz(qs as FormsDetails);
-
-          console.log(qs);
-
-          const answers = qs.questions.reduce((acc: { [key: string]: number }, q, i) => {
-            acc[q.$id] = qs.answers[i];
-            return acc;
-          }, {});
-
-          console.log(answers);
-
-          console.log(
-            'form init: ',
-            Object.entries(answers).reduce((acc: { [key: string]: string | undefined }, [key, value]) => {
-              acc[key] = value !== undefined ? qs?.questions.find(q => q.$id === key)?.answers[value] : undefined;
-              return acc;
-            }, {}),
-          );
-
-          const ans = Object.entries(answers).reduce((acc: { [key: string]: string | undefined }, [key, value]) => {
-            acc[key] = value !== undefined ? qs?.questions.find(q => q.$id === key)?.answers[value] : undefined;
-            return acc;
-          }, {});
-
-          setGivenAnswers(ans);
-          form.setValues(ans);
-        })
-        .catch(error => {
-          console.error(error);
-          setError(error as Error);
-        });
-    } catch (error) {
-      console.error(error);
-      setError(error as Error);
-    }
-  }, [quizId, getForm]);
-
-  const sendAnswers = useDebouncedCallback((givenAnswers: { [key: string]: number }) => {
-    const filtered = Object.entries(givenAnswers).reduce(
+  const saveAnswers = useDebouncedCallback((givenAnswers: { [key: string]: string | undefined }) => {
+    const answersWithIndex = getAnswersWithIndex(quiz?.questions, givenAnswers);
+    const filtered = Object.entries(answersWithIndex).reduce(
       (acc, [key, value]) => {
         if (value !== -1) {
           acc[key] = value;
@@ -112,7 +71,7 @@ export function FormsDetail() {
       answers: filtered,
     };
 
-    console.log('Sending answers', quizAnswers);
+    console.log('Saving answers', quizAnswers);
 
     functions
       .createExecution(
@@ -124,31 +83,20 @@ export function FormsDetail() {
       )
       .then(response => {
         if (response.status !== 'completed') {
-          throw new Error('Error sending answers');
+          throw new Error('Error saving answers');
         }
 
         console.log(response.responseBody);
       });
   }, 3000);
 
-  useEffect(() => {
-    sendAnswers(givenAnswers);
-  }, [givenAnswers, sendAnswers]);
-
   const sendResponse = async (values: { [key: string]: string | undefined }) => {
     setSubmitting(true);
 
-    const givenAnswers = Object.entries(values).reduce((acc: { [key: string]: number }, [key, value]) => {
-      acc[key] = value ? (quiz?.questions.find(q => q.$id === key)?.answers.indexOf(value) ?? -1) : -1;
-      return acc;
-    }, {});
+    const givenAnswers = getAnswersWithIndex(quiz?.questions, values);
 
     console.log('Submitting', givenAnswers);
     setSubmitting(false);
-
-    if (!false) {
-      return;
-    }
 
     const response = await functions.createExecution(
       '67120ae600174dc868f6',
@@ -167,17 +115,27 @@ export function FormsDetail() {
     console.log(response.responseBody);
   };
 
+  function getAnswersWithIndex(questions: TQuestion[] | undefined, answers: { [key: string]: string | undefined }) {
+    return Object.entries(answers).reduce((acc: { [key: string]: number }, [key, value]) => {
+      acc[key] = value ? (questions?.find(q => q.$id === key)?.answers.indexOf(value) ?? -1) : -1;
+      return acc;
+    }, {});
+  }
+
+  function getAnswersWithString(questions: TQuestion[] | undefined, answers: { [key: string]: number }) {
+    return Object.entries(answers).reduce((acc: { [key: string]: string | undefined }, [key, value]) => {
+      acc[key] = value !== -1 ? questions?.find(q => q.$id === key)?.answers[value] : undefined;
+      return acc;
+    }, {});
+  }
+
   const form = useForm({
     mode: 'uncontrolled',
-    initialValues: {
-      ...Object.entries(givenAnswers).reduce((acc: { [key: string]: number }, [key, value]) => {
-        acc[key] = value ? (quiz?.questions.find(q => q.$id === key)?.answers[value] ?? undefined) : undefined;
-        return acc;
-      }, {}),
-    },
+    initialValues: {},
 
     onValuesChange: values => {
       console.log('Values changed', values);
+      saveAnswers(values);
     },
 
     validate: quiz?.questions.reduce((acc: { [key: string]: (value: string | undefined) => string | undefined }, q) => {
@@ -191,6 +149,34 @@ export function FormsDetail() {
       return acc;
     }, {}),
   });
+
+  useEffect(() => {
+    try {
+      getForm(quizId)
+        .then(qs => {
+          setQuiz(qs as FormsDetails);
+
+          console.log(qs);
+
+          const answers = qs.questions.reduce((acc: { [key: string]: number }, q, i) => {
+            acc[q.$id] = qs.answers[i];
+            return acc;
+          }, {});
+
+          console.log(answers);
+
+          console.log('form init: ', getAnswersWithString(quiz?.questions, answers));
+          form.setValues(getAnswersWithString(quiz?.questions, answers));
+        })
+        .catch(error => {
+          console.error(error);
+          setError(error as Error);
+        });
+    } catch (error) {
+      console.error(error);
+      setError(error as Error);
+    }
+  }, [quizId, getForm, quiz?.questions, form]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
